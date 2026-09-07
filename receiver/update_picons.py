0180491c6d3210ba54ca51e5e11875398c27e361
JohnySmith <42406628+GrgoPitic@users.noreply.github.com>
2026-09-07 21:38:03 +0200

    Allow receiver updater to select picon group

--- receiver/update_picons.py
+++ receiver/update_picons.py
@@ -9,13 +9,20 @@ import io, json, os, shutil, sys, urllib.request, zipfile
 
 BASE_URL = "https://grgopitic.github.io/satellite-picons"
 DEST = sys.argv[1] if len(sys.argv) > 1 else "/media/hdd/picon"
+GROUP = sys.argv[2] if len(sys.argv) > 2 else ""
 
 def get(url):
     with urllib.request.urlopen(url, timeout=30) as r:
         return r.read()
 
 version=json.loads(get(BASE_URL.rstrip('/')+'/version.json'))
-blob=get(BASE_URL.rstrip('/')+'/'+version['package'])
+package=version['package']
+if GROUP:
+    group=version.get('groups',{}).get(GROUP)
+    if not group:
+        raise SystemExit('Unknown picon group: '+GROUP)
+    package=group['package']
+blob=get(BASE_URL.rstrip('/')+'/'+package)
 os.makedirs(DEST,exist_ok=True)
 with zipfile.ZipFile(io.BytesIO(blob)) as z:
     for info in z.infolist():
